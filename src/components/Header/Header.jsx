a999c08927f5f2df64ba67a610c9bc72febf547f
Tony <tonymike99@gmail.com>
2023-01-25 02:24:29 +0530

    added Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <span className="app-name">HOURS</span>
+      <nav className="navbar">
+        <a href="/">Dashboard</a>
+        <a href="/">Projects</a>
+        <a href="/">Team</a>
+        <a href="/">Clients</a>
+        <a href="/">Time</a>
+        <a href="/">Reports</a>
+      </nav>
+      <div className="settings">
+        <span>
+          <i className="fa-regular fa-bell fa-2x"></i>
+        </span>
+        <img
+          className="display-picture"
+          src="https://i.pravatar.cc/30?img=1"
+          alt="woman"
+        />
+        <span>Mario</span>
+        <span>
+          <i className="fa-solid fa-chevron-down fa-lg"></i>
+        </span>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
